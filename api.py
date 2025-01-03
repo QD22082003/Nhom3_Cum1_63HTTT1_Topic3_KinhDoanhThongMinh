bb14b94b4276efc53fd73271cbb1e50dca8ecafc
Nguyễn Ngọc Bách <144202616+Bach293@users.noreply.github.com>
2025-01-03 21:30:24 +0700

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ generation_config = {
     "temperature": 1,
     "top_p": 0.95,
     "top_k": 40,
-    "max_output_tokens": 8192,
+    "max_output_tokens": 1024,
     "response_mime_type": "text/plain",
 }
 
